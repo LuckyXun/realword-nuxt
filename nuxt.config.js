6ec0c848e082750b41be35f62470ec8c0867ab98
XunL <18380422673@163.com>
2021-07-08 10:50:17 +0800

    feat：命令配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,6 +3,10 @@ export default {
         title: 'real - world',
      
     },
+    server: {
+        host: '0.0.0.0',
+        port: 3000
+    },
     router: {
         linkExactActiveClass: "active",
         extendRoutes(routes, resolve) {
